7c2c7bd2e4d2f1f64244f2d4fb514577c6080cf0
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-10-25 16:43:07 -0300

    created modal in header mobile

---
+++ Js/modal_header.js
@@ -0,0 +1,12 @@
+const open_modal = document.querySelector('.open-icon');
+const close_modal = document.querySelector('.close-icon');
+
+open_modal.addEventListener('click', function() {
+    open_modal.style.display = 'none';
+    close_modal.style.display = 'block';    
+});
+
+close_modal.addEventListener('click', function(){
+    close_modal.style.display = 'none';
+    open_modal.style.display = 'block';
+});
