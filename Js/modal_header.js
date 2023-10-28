d07a2d62cf63d6dac98d40688a132ccea80985ff
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-10-28 16:34:05 -0300

    progress menu mobile

--- Js/modal_header.js
+++ Js/modal_header.js
@@ -1,12 +1,17 @@
 const open_modal = document.querySelector('.bi-bi-list');
 const close_modal = document.querySelector('.close-icon');
+const open_menu_mobile = document.querySelector('.nav-mobile-icons')
+const close_menu_mobile = document.querySelector('.nav-mobile-icons')
 
 open_modal.addEventListener('click', function() {
     open_modal.style.display = 'none';
-    close_modal.style.display = 'block';    
+    close_modal.style.display = 'block';   
+    open_menu_mobile.style.display = 'block';
 });
 
 close_modal.addEventListener('click', function(){
     close_modal.style.display = 'none';
     open_modal.style.display = 'block';
+    close_menu_mobile.style.display = 'none';
 });
+
