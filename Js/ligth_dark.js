18daa567e45a501c9c8e85d210939d2278581c16
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-11-04 10:24:31 -0300

    color adjustments in the dark version

--- Js/ligth_dark.js
+++ Js/ligth_dark.js
@@ -1,5 +1,8 @@
 let modo_light_dark = document.querySelector('.light_dark');
 
 modo_light_dark.addEventListener('click', () => {
-    modo_light_dark.classList.toggle('dark');
+    modo_light_dark.classList.toggle('dark')
+    body.classList.toggle('dark')
+    header.classList.toggle('dark')
 });
+
