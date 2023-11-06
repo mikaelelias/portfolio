c1fe97375c6b832948d60577c77c80a77135df3d
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-11-06 16:02:40 -0300

    code refactoring

---
+++ Js/ligth_dark.js
@@ -0,0 +1,8 @@
+const openModalEl = document.querySelector('.open-icon');
+
+modo_light_dark.addEventListener('click', () => {
+    modo_light_dark.classList.toggle('dark')
+    body.classList.toggle('dark')
+    header.classList.toggle('dark')
+});
+
