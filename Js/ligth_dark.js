943049dba73bade4e90eef86b615fdf22cb7145a
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-11-07 15:58:51 -0300

    upgrad

--- Js/ligth_dark.js
+++ Js/ligth_dark.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 let modo_light_dark = document.querySelector('.light_dark');
-=======
 const openModalEl = document.querySelector('.open-icon');
->>>>>>> mobile-contact
 
 modo_light_dark.addEventListener('click', () => {
     modo_light_dark.classList.toggle('dark')
