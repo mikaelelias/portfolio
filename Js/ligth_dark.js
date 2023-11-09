ef1b3734952450b915a6b4e3ef082deea06eb9fa
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-11-09 20:10:39 -0300

    progress in modo dark

--- Js/ligth_dark.js
+++ Js/ligth_dark.js
@@ -5,5 +5,8 @@ modo_light_dark.addEventListener('click', () => {
     modo_light_dark.classList.toggle('dark')
     body.classList.toggle('dark')
     header.classList.toggle('dark')
-});
+}); 
+
+
+
 
