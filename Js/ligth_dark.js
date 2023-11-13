365b7dd6faa9f2a82f2358ab079db8d8ebe9c59f
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-11-13 17:27:23 -0300

    Light/Dark session finished

--- Js/ligth_dark.js
+++ Js/ligth_dark.js
@@ -1,9 +1,10 @@
-let modo_light_dark = document.querySelector('.light_dark');
-const openModalEl = document.querySelector('.open-icon');
+const modo_light_dark = document.querySelector('.icon-moon');
 
 modo_light_dark.addEventListener('click', () => {
     modo_light_dark.classList.toggle('dark')
     body.classList.toggle('dark')
     header.classList.toggle('dark')
 });
+
+
 
