d5cbb1e979724bd361a5187f077365b9e789cd65
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-11-17 16:32:49 -0300

    code refactoring

--- Js/light_dark.js
+++ Js/light_dark.js
@@ -1,9 +1,7 @@
 const modo_light_dark = document.querySelector('.icon-moon');
 
 modo_light_dark.addEventListener('click', () => {
-    modo_light_dark.classList.toggle('dark')
     body.classList.toggle('dark')
-    header.classList.toggle('dark')
 });
 
 
