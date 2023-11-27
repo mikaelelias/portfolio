f2239c0822dd2898e263833ebd865ef5c871750d
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-11-27 16:50:29 -0300

    application of the first part of the translator

---
+++ Js/translations.js
@@ -0,0 +1,42 @@
+const languageButton = document.querySelector('.language-button');
+
+const translations = {
+    en: {
+        title: `Hi 👋, <br> My name is <br><span class="highlight">Mikael Elias</span><br>
+        and I build things for web.`,
+        menuItem1: "Home",
+        menuItem2: "About",
+        menuItem3: "Contact",
+    },
+    pt: {
+        title: `Olá 👋, <br> Meu nome é <span class="highlight">Mikael Elias</span><br>e eu desenvolvo<br>coisas para a Web.`,
+        menuItem1: "Home",
+        menuItem2: "Sobre",
+        menuItem3: "Contato",
+    }
+}
+
+function updateLanguage() {
+    Object.keys(translations[getCurrentLang()]).forEach(key => {
+        const element = document.querySelector(`[data-translation-key=${key}]`);
+        if (!element) return;
+        element.innerHTML = translations[getCurrentLang()][key];
+    })
+}
+
+function getCurrentLang() {
+    return localStorage.getItem("lang") ?? "en"
+}
+
+function toogleLanguage() {
+    localStorage.setItem("lang", getCurrentLang() === 'en' ? 'pt' : 'en');
+}
+
+function onClickLanguageButton() {
+    toogleLanguage();
+    updateLanguage();
+}
+
+languageButton.addEventListener('click', onClickLanguageButton)
+
+updateLanguage();
