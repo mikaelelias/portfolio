8400575c2e158c9a4575a307b30140e4928c218c
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-11-29 15:59:23 -0300

    progress in translate

--- Js/translations.js
+++ Js/translations.js
@@ -9,6 +9,15 @@ const translations = {
         menuItem3: "Contact",
         titleTechnologies: "My Tech Stack",
         descriptionTechnologies: "Technologies I've been working with recently",
+        sectionTitleProjects: "Projects",
+        sectionDescriptionProjects: "Things I've built so far",
+        cardProjectTitle: "Project Tile goes here",
+        cardProjectTitleDescription: "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
+        techStack: "<b>Tech stack:</b> HTML, JavaScript, SASS, React</p>",
+        menuItem1Footer: "Home",
+        menuItem2Footer: "About",
+        menuItem3Footer: "Contact",
+        /*copyright: "Designed by Pavan MG and built by Mikael Elias",*/
     },
     pt: {
         title: `Olá 👋, <br> Meu nome é <span class="highlight">Mikael Elias</span><br>e eu desenvolvo<br>coisas para a Web.`,
@@ -17,6 +26,15 @@ const translations = {
         menuItem3: "Contato",
         titleTechnologies: "Minhas Tecnologias",
         descriptionTechnologies: "Tecnologias com as quais tenho trabalhado recentemente",
+        sectionTitleProjects: "Projetos",
+        sectionDescriptionProjects: "Coisas que construí até agora",
+        cardProjectTitle: "O bloco do projeto vai aqui",
+        cardProjectTitleDescription: "Este é um exemplo de descrição do projeto, coisas aleatórias estão aqui na descrição Este é um exemplo de projeto, gerador lorem ipsum para conteúdo fictício",
+        techStack: "<b>Tecnologia:</b> HTML, JavaScript, SASS, React</p>",
+        menuItem1Footer: "Home",
+        menuItem2Footer: "Sobre",
+        menuItem3Footer: "Contato",
+        /*copyright: "Projetado por Pavan MG e construído por Mikael Elias",*/
     }
 }
 
